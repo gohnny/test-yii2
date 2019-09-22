0ce68d1060db9d67a803580af3811259504d88b1
gohnny <gohnny@gmail.com>
2019-09-22 12:14:49 +0300

    add web page test main,join,login

--- tests/acceptance/TestGuestPagesCept.php
+++ tests/acceptance/TestGuestPagesCept.php
@@ -3,3 +3,11 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('Open home');
 $I->amOnPage('/');
 $I->see('Hello from render index');
+$I->seeLink('Join', '/site/join');
+$I->seeLink('Login', '/site/login');
+
+$I->amOnPage('/site/join');
+$I->see('Join us');
+
+$I->amOnPage('/site/login');
+$I->see('Log in');
