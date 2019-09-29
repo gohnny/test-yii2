913f6cd2091b7abcea0886fa408def016eb06bcb
gohnny <gohnny@gmail.com>
2019-09-29 12:16:14 +0300

    add css,js; include bootstrap4; add content

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,7 +1,50 @@
-<html>
-<head>
-</head>
-<body>
-<?= $content ?>
-</body>
-</html>
+<?php
+
+use app\assets\AppAsset;
+use yii\helpers\Html;
+use yii\mail\MessageInterface;
+use yii\web\View;
+
+/* @var $this View view component instance */
+/* @var $message MessageInterface the message being composed */
+/* @var $content string main view render result */
+?>
+<?php AppAsset::register($this); ?>
+<?php $this->beginPage() ?>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>"/>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+    <?php $this->beginBody() ?>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg fixed-top ">
+        <a class="navbar-brand" href="#">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav mr-4">
+                <li class="nav-item">
+                    <a class="nav-link" data-value="about" href="#">About</a></li>
+                <li class="nav-item">
+                    <a class="nav-link " data-value="portfolio" href="#">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " data-value="blog" href="#">Blog</a></li>
+                <li class="nav-item">
+                    <a class="nav-link " data-value="team" href="#"> Team</a></li>
+                <li class="nav-item">
+                    <a class="nav-link " data-value="contact" href="#">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+    <?= $content ?>
+    <?php $this->endBody() ?>
+    </body>
+    </html>
+<?php $this->endPage() ?>
--- views/site/index.php
+++ views/site/index.php
@@ -1,1 +1,201 @@
-<h1> 'Hello from render index'</h1>
+<header class="header">
+    <div class="overlay"></div>
+    <div class="description  ">
+        <h1> Hello ,Welcome To My official Website
+            <p> cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <button class="btn btn-outline-secondary btn-lg">See more</button>
+        </h1>
+    </div>
+</header>
+<!-- Контент страницы -->
+<div class="about" id="about"></div>
+<div class="row about">
+    <div class="col-lg-4 col-md-4 col-sm-12">
+        <img src="images/team/team-3.jpg" class="img-fluid">
+        <span class="text-justify">S.Web Developer</span>
+    </div>
+    <div class="col-lg-8 col-md-8 col-sm-12 desc">
+
+        <h3>D.John</h3>
+        <p>
+            ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+</div>
+<!-- portfolio -->
+<div class="portfolio" id="portfolio"></div>
+<div class="portfolio">
+    <h1 class="text-center">Portfolio</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <img src="images/portfolio/bear-4494180__340.jpg" class="img-fluid">
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <img src="images/portfolio/cello-4480885__340.jpg" class="img-fluid">
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <img src="images/portfolio/dusk-3745864__340.jpg" class="img-fluid">
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <img src="images/portfolio/sheep-4418342__340.jpg" class="img-fluid">
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <img src="images/portfolio/squirrel-4466632__340.jpg" class="img-fluid">
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <img src="images/portfolio/sunrise-4497745__340.jpg" class="img-fluid">
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <img src="images/portfolio/takeoff-4471092__340.jpg" class="img-fluid">
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <img src="images/portfolio/sunrise-4497745__340.jpg" class="img-fluid">
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <img src="images/portfolio/sheep-4418342__340.jpg" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Posts section -->
+<div class="blog" id="blog">
+    <div class="container">
+        <h1 class="text-center">Blog</h1>
+        <div class="row">
+            <div class="col-md-4 col-lg-4 col-sm-12">
+                <div class="card">
+                    <div class="card-img">
+                        <img src="images/posts/polit.jpg" class="img-fluid">
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title">Post Title</h4>
+                        <p class="card-text">
+
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="" class="card-link">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 col-sm-12">
+                <div class="card">
+                    <div class="card-img">
+                        <img src="images/posts/images.jpg" class="img-fluid">
+                    </div>
+
+                    <div class="card-body">
+                        <h4 class="card-title">Post Title</h4>
+                        <p class="card-text">
+
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="" class="card-link">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 col-sm-12">
+                <div class="card">
+                    <div class="card-img">
+                        <img src="images/posts/imag2.jpg" class="img-fluid">
+                    </div>
+
+                    <div class="card-body">
+                        <h4 class="card-title">Post Title</h4>
+                        <p class="card-text">
+
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="" class="card-link">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Team section -->
+<div class="team" id="team">
+    <div class="container">
+        <h1 class="text-center">Our Team</h1>
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-12 item">
+                <img src="images/team/team-3.jpg" class="img-fluid" alt="team">
+                <div class="des">
+                    Sara
+                </div>
+                <span class="text-muted">Manager</span>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 item">
+                <img src="images/team/team-3.jpg" class="img-fluid" alt="team">
+                <div class="des">
+                    Chris
+                </div>
+                <span class="text-muted">S.enginner</span>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 item">
+                <img src="images/team/team-3.jpg" class="img-fluid" alt="team">
+                <div class="des">
+                    Layla
+                </div>
+                <span class="text-muted">Front End Developer</span>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 item">
+                <img src="images/team/team-3.jpg" class="img-fluid" alt="team">
+                <div class="des">
+                    J.Jirard
+                </div>
+                <span class="text-muted">Team Manger</span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Contact form -->
+<div class="contact-form" id="contact">
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <h1>Get in Touch</h1>
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-12 right">
+                    <div class="form-group">
+                        <input type="text" class="form-control form-control-lg" placeholder="Your Name" name="">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control form-control-lg" placeholder="YourEmail@email.com"
+                               name="email">
+                    </div>
+                    <div class="form-group">
+         <textarea class="form-control form-control-lg">
+
+         </textarea>
+                    </div>
+                    <input type="submit" class="btn btn-secondary btn-block" value="Send" name="">
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<!-- Подключаем jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- Подключаем плагин Popper (необходим для работы компонента Dropdown и др.) -->
+<script src="js/popper.min.js"></script>
+<!-- Подключаем Bootstrap JS -->
+<script src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src='js/main.js'></script>
