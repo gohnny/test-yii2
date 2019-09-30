161063541c6c07356b6f91d5c6248045b941e515
gohnny <gohnny@gmail.com>
2019-09-30 19:35:22 +0300

    remove old bootstrap,add yii-debug module; require autoload

--- config/web.php
+++ config/web.php
@@ -2,10 +2,17 @@
 return [
     'id' => 'test',
     'basePath' => realpath(__DIR__ . '/../'),
+    'bootstrap' => ['debug'],
     'components' => [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false
+        ],
+        'request' => [
+            'cookieValidationKey' => 'super secret code 12123151465446542'
         ]
+    ],
+    'modules' => [
+        'debug' => ['class' => 'yii\debug\Module']
     ]
 ];
