2e05908111a5971a82860d07c65aeddb3e724736
gohnny <gohnny@gmail.com>
2019-10-13 17:29:43 +0300

    refactoring main layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,7 +4,8 @@ use app\assets\AppAsset;
 use yii\helpers\Html;
 use yii\mail\MessageInterface;
 use yii\web\View;
-use yii\helpers\Url;
+use yii\bootstrap4\NavBar;
+use yii\bootstrap4\Nav;
 
 /* @var $this View view component instance */
 /* @var $message MessageInterface the message being composed */
@@ -13,41 +14,50 @@ use yii\helpers\Url;
 <?php AppAsset::register($this); ?>
 <?php $this->beginPage() ?>
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+    <html lang="<?= Yii::$app->language ?>">
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>"/>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php $this->registerCsrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
     </head>
+    <?php $this->head() ?>
     <body>
     <?php $this->beginBody() ?>
     <!-- navbar -->
-    <nav class="navbar navbar-expand-lg fixed-top ">
-        <a class="navbar-brand" href="<?= Yii::$app->homeUrl ?>">Home</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse " id="navbarSupportedContent">
-            <ul class="navbar-nav mr-4">
-                <li class="nav-item">
-                    <a class="nav-link" data-value="about" href="#">About</a></li>
-                <li class="nav-item">
-                    <a class="nav-link " data-value="portfolio" href="#">Portfolio</a></li>
-                <li class="nav-item">
-                    <a class="nav-link " data-value="blog" href="#">Blog</a></li>
-                <li class="nav-item">
-                    <a class="nav-link " data-value="contact"
-                       href="<?= Yii::$app->urlManager->createUrl(['site/feedback-form']) ?>">Contact</a></li>
-                <li class="nav-item">
-                    <a class="nav-link " data-value="join"
-                       href="<?= Yii::$app->urlManager->createUrl(['user/join']) ?>">Join</a></li>
-                <li class="nav-item">
-                    <a class="nav-link " data-value="login"
-                       href="<?= Yii::$app->urlManager->createUrl(['user/login']) ?>">Login</a></li>
-            </ul>
-        </div>
-    </nav>
+    <?php
+    NavBar::begin([
+        'id' => 'nav',
+        'brandLabel' => 'Home',
+        'brandUrl' => Yii::$app->homeUrl,
+        'brandOptions' => ['class' => 'navbar-brand'],
+        'renderInnerContainer' => false,
+        // 'innerContainerOptions' =>['class'=>'navbar navbar-expand-lg fixed-top'],
+        'options' => ['class' => 'navbar navbar-expand-lg fixed-top'],
+
+    ]);
+    if (Yii::$app->user->isGuest)
+        $menu = [
+            ['label' => 'Join', 'url' => ['user/join']],
+            ['label' => 'Login', 'url' => ['user/login']],
+            ['label' => 'Contact', 'url' => ['site/feedback-form']],
+        ];
+    else
+        $menu = [
+            ['label' => 'Contact', 'url' => ['site/feedback-form']],
+            ['label' => Yii::$app->user->getIdentity()->name],
+            ['label' => 'Logout', 'url' => ['user/logout']],
+        ];
+    echo Nav::widget([
+        'options' => [
+            'class' => 'navbar-nav mr-4',
+        ],
+        'items' => $menu
+    ]);
+    NavBar::end()
+    ?>
     <?= $content ?>
     <?php $this->endBody() ?>
     </body>
